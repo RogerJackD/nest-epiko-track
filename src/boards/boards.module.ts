d3a1687687e3ff2656fcd89580e0ea6c545716bb
RogerJackD <rogerzapata114@gmail.com>
2025-09-18 23:42:06 -0500

    feat(boards): board entity added

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { BoardsController } from './boards.controller';
+import { Board } from './entities/board.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [BoardsController],
   providers: [BoardsService],
+  imports: [
+    TypeOrmModule.forFeature([Board]),
+  ],
 })
 export class BoardsModule {}
--- src/boards/entities/board.entity.ts
+++ src/boards/entities/board.entity.ts
@@ -1,1 +1,31 @@
-export class Board {}
+import { CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn, Entity, Column } from 'typeorm';
+
+@Entity()
+export class Board {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({
+        type: 'text',
+    })
+    name: string;
+
+    @Column({
+        type: 'text',
+    })
+    description: string;
+
+    @CreateDateColumn({
+        type: 'timestamp'
+    })
+    createdAt: Date;
+
+    @UpdateDateColumn({
+        type: 'timestamp'
+    })
+    updatedAt: Date;
+
+    //todo: crear estados para tableros
+
+}
