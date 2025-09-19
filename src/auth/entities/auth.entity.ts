7b8f89b92a4821a2bf80b06b50107f0f2ea6edf1
RogerJackD <rogerzapata114@gmail.com>
2025-09-18 23:37:19 -0500

    feat(auth): users entity added and column fullName_normalized with extra method

--- src/auth/entities/auth.entity.ts
+++ src/auth/entities/auth.entity.ts
@@ -1,1 +1,70 @@
-export class Auth {}
+import { BeforeInsert, BeforeUpdate, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({   
+        type: 'text',
+    })
+    firstName: string;
+
+    @Column({
+        type: 'text',
+    })
+    lastName: string;
+
+    @Column('int',{
+        nullable: true
+    })
+    age: number;
+
+    @Column('text',{
+        nullable: true,
+        unique: true,
+    })
+    email: string;
+
+    @CreateDateColumn({
+        type: 'timestamp'
+    })
+    createdAt: Date;
+
+    @UpdateDateColumn({
+        type: 'timestamp'
+    })
+    updatedAt: Date;
+
+    @Column('timestamp',{
+        nullable: true
+    })
+    contractDate: Date;
+
+    //todo: crear idRol->Rol
+    //todo: crear idArea->Area
+
+    @Column({
+        type: 'text',
+    })
+    password: string
+
+    @Column('int',{
+        default: 1
+    })
+    status : number
+
+    @Column('varchar')
+    fullName_normalized: string
+
+    @BeforeUpdate()
+    @BeforeInsert()
+    normalizeName(){
+        this.fullName_normalized = `${this.firstName} ${this.lastName}`
+            .toLowerCase()
+            .normalize('NFD')
+            .replace(/[\u0300-\u036f]/g, "") //eliminar acentos
+            .replace(/\s+/g, " ") //normalizar espacios
+    }
+}
