dbcdacb359e5a124d6c4453f1a5bf20ef94746ce
RogerJackD <rogerzapata114@gmail.com>
2025-10-16 19:14:25 -0500

    feat(boards): extend property for update status, new property taskStatusId

--- src/boards/dto/update-task.dto.ts
+++ src/boards/dto/update-task.dto.ts
@@ -1,6 +1,10 @@
 import { PartialType } from "@nestjs/mapped-types";
 import { CreateTaskDto } from "./create-task.dto";
+import { IsNumber, IsOptional } from "class-validator";
 
 export class UpdateTaskDto extends PartialType(CreateTaskDto) {
 
+    @IsOptional()
+    @IsNumber()
+    taskStatusId: number;
 }
