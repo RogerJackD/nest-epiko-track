337c285e4357664941ba81f00ccc6a473b4e8114
RogerJackD <rogerzapata114@gmail.com>
2025-10-27 22:38:47 -0500

    feat(auth): add class with logic JwtStragey and validation user

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,40 @@
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { InjectRepository } from "@nestjs/typeorm";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { User } from "../entities/auth.entity";
+import { Repository } from "typeorm";
+import { ConfigService } from "@nestjs/config";
+import { JwtPayload } from "../interfaces/jwt-payload.interface";
+
+
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy( Strategy ){
+
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+        ConfigService: ConfigService,
+    ){
+        super({
+            secretOrKey: ConfigService.get<string>('JWT_SECRET') as string,
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+        })
+    }
+
+
+    async validate(payload: JwtPayload): Promise<User> {
+
+        const { id } = payload;
+
+        const user = await this.userRepository.findOneBy({ id });
+
+        if ( !user ) 
+            throw new UnauthorizedException('User not found - JWT Strategy');
+
+        return user;
+
+    }
+
+}
