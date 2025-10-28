72b06e41e3862f6d2d35fa142b3b6a383f2d39f1
RogerJackD <rogerzapata114@gmail.com>
2025-10-27 22:23:33 -0500

    feat(auth): add config jwt module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,12 +4,36 @@ import { AuthController } from './auth.controller';
 import { User } from './entities/auth.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Role } from './entities/role.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
   imports: [
+    
     TypeOrmModule.forFeature([User, Role]),
+
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+
+    JwtModule.registerAsync({
+
+      imports: [ConfigModule],
+      inject: [ConfigService],
+
+      useFactory: (configService: ConfigService) => {
+        
+        return {
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: '2h'}
+        };
+      },
+    }),
+
   ],
+
+  exports: [TypeOrmModule, PassportModule, JwtModule],
 })
 export class AuthModule {}
--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsDateString, IsEmail, IsNumber, IsOptional, IsPhoneNumber, IsString } from "class-validator";
+import { IsDateString, IsEmail, IsNumber, IsOptional, IsPhoneNumber, IsString, Matches } from "class-validator";
 
 export class CreateUserDto {
 
@@ -31,5 +31,11 @@ export class CreateUserDto {
     @IsPhoneNumber('PE')
     @IsOptional()
     phoneNumber: string;
+
+    @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'The password must have a Uppercase, lowercase letter and a number',
+    })
+    password: string;
     
 }
