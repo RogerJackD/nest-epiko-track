1b8550641f35aed2a3efdfc820383fb1aa594833
RogerJackD <rogerzapata114@gmail.com>
2025-11-03 21:14:51 -0500

    feat(boards): create DTO for create board

--- src/boards/dto/create-board.dto.ts
+++ src/boards/dto/create-board.dto.ts
@@ -1,1 +1,13 @@
-export class CreateBoardDto {}
+import { IsNumber, IsString } from "class-validator";
+
+export class CreateBoardDto {
+
+    @IsString()
+    title: string;
+
+    @IsString()
+    description: string;
+
+    @IsNumber()
+    areaId: number;
+}
