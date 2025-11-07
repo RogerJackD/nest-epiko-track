9aba3755764619c69b4a531aacfc25ba6c277b38
RogerJackD <rogerzapata114@gmail.com>
2025-11-07 11:17:48 -0500

    feat(boards): add UpdateBoardDto extending CreateBoardDto with optional isActive field

--- src/boards/dto/update-board.dto.ts
+++ src/boards/dto/update-board.dto.ts
@@ -1,5 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateBoardDto } from './create-board.dto';
+import { IsOptional } from 'class-validator';
 
 export class UpdateBoardDto extends PartialType(CreateBoardDto) {
+    @IsOptional()
+    isActive?: boolean;
 }
